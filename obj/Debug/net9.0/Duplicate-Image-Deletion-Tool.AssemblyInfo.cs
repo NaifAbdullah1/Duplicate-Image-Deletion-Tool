13153072c9fa6e8bbe1651a263daf1e88ed16db5
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-04-19 17:15:32 +0300

    No longer using libraries, attempting to use perceptual hashes instead of dHashes

--- obj/Debug/net9.0/Duplicate-Image-Deletion-Tool.AssemblyInfo.cs
+++ obj/Debug/net9.0/Duplicate-Image-Deletion-Tool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Duplicate-Image-Deletion-Tool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd88076008286e67aa137ac27a45a73c4f000658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d3614981b58b7a318ec2cb3afb89a23a0c3f8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Duplicate-Image-Deletion-Tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Duplicate-Image-Deletion-Tool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
